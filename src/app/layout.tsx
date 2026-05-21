41c432a40ae173723b47d288b0a2fe843a97cbfe
Mreena Kousar <mrina9035@gmail.com>
2026-05-21 14:31:29 -0700

    final-lastcodelocalregular

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import { Toaster } from "react-hot-toast";
+import { Providers } from "@/components/Providers";
+import { LayoutShell } from "@/components/layout/LayoutShell";
+import { AuthProvider } from "@/components/context/AuthContext";
 
 
 const geistSans = Geist({
@@ -19,9 +22,7 @@ export const metadata: Metadata = {
   description: "Experience weightless shopping",
 };
 
-import { Providers } from "@/components/Providers";
-import { LayoutShell } from "@/components/layout/LayoutShell";
-import { AuthProvider } from "@/components/context/AuthContext";
+
 
 export default function RootLayout({
   children,
@@ -32,6 +33,8 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning={true}
+      
     >
       <body className="min-h-full flex flex-col relative">
         <div className="fixed inset-0 pointer-events-none bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-900 via-black to-black opacity-80 -z-10"></div>
